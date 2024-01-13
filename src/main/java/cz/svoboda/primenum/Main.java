d5ad58abfd709bf9933337796466854c5ab12006
Martin Svoboda <svoboda.martin.91@gmail.com>
2024-01-13 21:25:44 +0100

    deleted throw exception

--- src/main/java/cz/svoboda/primenum/Main.java
+++ src/main/java/cz/svoboda/primenum/Main.java
@@ -1,10 +1,9 @@
 package cz.svoboda.primenum;
 
 import cz.svoboda.primenum.app.Application;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Application app = new Application(args);
         app.run();
     }
